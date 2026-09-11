e8b0ce73bd17a8231a8816083448398c5b112777
Gianni Bautista <111990625+lilyanniii@users.noreply.github.com>
2026-09-11 08:38:42 -0400

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+
+        string = str(x)
+
+        l, r = 0, len(string) - 1
+
+        while l < r:
+            if string[l] != string[r]:
+                return False
+            else:
+                l += 1
+                r -= 1
+        
+        return True
