5515dc828062d2e70cc739891533d60b05d3afed
Gianni Bautista <111990625+lilyanniii@users.noreply.github.com>
2026-09-22 20:55:47 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_table = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            hash_table[tuple(count)].append(s)
+        
+        return list(hash_table.values())
